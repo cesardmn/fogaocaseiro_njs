b7dfa3e5cb9ba18ab207c474ab17f609aca3c9ce
Cesar <cesardmn@gmail.com>
2021-02-01 07:20:46 -0300

    include token enviroment variable

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-module.exports = { 
-  env: { 
-    BACK_API: process.env.BACK_API, 
-  }, 
+module.exports = {
+  env: {
+    BACK_API: process.env.BACK_API,
+    BACK_API_DEV: process.env.BACK_API_DEV,
+    TOKEN: process.env.TOKEN,
+  },
 };
