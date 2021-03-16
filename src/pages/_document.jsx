9627d38fdd13173ed8965274051a1e7fe5d9eb48
Cesar <cesardmn@gmail.com>
2021-03-15 22:12:14 -0300

    change browser bar  color

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
       <Html lang="pt-br">
         <Head>
           {/* PWA primary color */}
-          <meta name="theme-color" content={theme.palette.primary.main} />
+          {/* <meta name="theme-color" content={theme.palette.grey} /> */}
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
