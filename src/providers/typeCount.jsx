ee7c30fd1b79921fc4559cdf914f07ed0048fc62
Cesar <cesardmn@gmail.com>
2021-03-15 22:12:51 -0300

    include news states

--- src/providers/typeCount.jsx
+++ src/providers/typeCount.jsx
@@ -4,9 +4,20 @@ export const TypeContext = createContext({})
 
 export const TypeProvider = (props) => {
   const [totalType, setTotalType] = useState(0)
+  const [totalValueType, setTotalValueType] = useState(0)
+  const [totalCountType, setTotalCountType] = useState(0)
 
   return (
-    <TypeContext.Provider value={{ totalType, setTotalType }}>
+    <TypeContext.Provider
+      value={{
+        totalType,
+        setTotalType,
+        totalValueType,
+        setTotalValueType,
+        totalCountType,
+        setTotalCountType,
+      }}
+    >
       {props.children}
     </TypeContext.Provider>
   )
